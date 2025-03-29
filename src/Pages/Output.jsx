6aeae6e41fa63c595e7305b69d6fb5bba1e873a2
Althafvc <aljaitrainternational@gmail.com>
2025-03-29 14:35:10 +0530

    searchbar removed

--- src/Pages/Output.jsx
+++ src/Pages/Output.jsx
@@ -12,11 +12,11 @@ function Output() {
   return (
 
     <div className="wrapper h-screen w-screen">
-      <div className="navbar w-full h-[80px] bg-slate-500 flex justify-center items-center gap-4  px-4">
+      {/* <div className="navbar w-full h-[80px] bg-slate-500 flex justify-center items-center gap-4  px-4">
         <SearchIcon style={{ color: "red", fontSize: "40px" }} />
         <input type="text" name="" id="" className='w-[60%] h-[40%] rounded-sm outline-none pl-4 pr-3 font-kanitFont' />
         <button type='submit' className='py-2 px-4 bg-[blue] rounded-md text-white font-semibold'>Search</button>
-      </div>
+      </div> */}
 
 
 
@@ -41,7 +41,6 @@ function Output() {
             <p className='font-semibold  font-kanitFont text-[18px] md:text-[22px] text-white underline decoration-2 underline-offset-4'>SPECIALISATIONS</p>
 
             <div className="spwrapper flex flex-col gap-4 w-full justify-around px-4 md:px-6 py-2">
-              {/* <div className="coursewrapper w-full border-2 flex flex-col gap-4"> */}
               {searchData.SPECIALISATIONS &&
                 searchData.FEESAMOUNT &&
                 searchData.SPECIALISATIONS.map((specialisation, index) => (
@@ -54,14 +53,6 @@ function Output() {
           </div>
 
           <div className="coursename bg-[#008FE8] flex justify-between px-4 md:px-6 items-center py-2 rounded-[6px]"> <p className='font-semibold  font-kanitFont text-[15px] md:text-[22px] text-white'>Location </p>   <p className='font-semibold font-kanitFont text-[15px] text-white md:text-[22px]'>{searchData.LOCATION}</p> </div>
-
-
-
-
-
-
-
-
 
         </div>
       </div>
